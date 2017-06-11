622f5c1b85f4408e7f535ee7af6b922a6f8ea0e6
Toanngo <toanngosy@gmail.com>
2017-06-11 16:05:01 +0200

    fix relation.cpp

--- relation.cpp
+++ relation.cpp
@@ -4,8 +4,8 @@
 
 void Relation::ajouterCouple(Couple* newCouple){
     if (nbCouples == maxCouples){
-        Couple** newCouples = new Couple[maxCouples+10];
-        for (int i=0; i < nbCouples; i++) newCouples[i] = couples[i];
+        Couple** newCouples = new Couple*[maxCouples+10];
+        for (unsigned int i=0; i < nbCouples; i++) newCouples[i] = couples[i];
         Couple** oldCouples = couples;
         couples = newCouples;
         delete[] oldCouples;
@@ -16,12 +16,12 @@ void Relation::ajouterCouple(Couple* newCouple){
 
 void Relation::supprimerCouple(Couple* supCouple){
     int pos = -1;
-    for (int i=0; i < nbCouples; i++){
+    for (unsigned int i=0; i < nbCouples; i++){
         if (couples[i] == supCouple) pos = i;
     }
 
     if (pos != -1){
-        for (int i=pos; i < nbCouples-1; i++){
+        for (unsigned int i=pos; i < nbCouples-1; i++){
             couples[i] = couples[i+1];
         }
         delete supCouple;
@@ -31,11 +31,11 @@ void Relation::supprimerCouple(Couple* supCouple){
 
 RelationPreexistente* RelationPreexistente::instance_RelationPreexistente = 0;
 
-void RelationManager::ajouterRelation(Couple* newRelation){
+void RelationManager::ajouterRelation(Relation* newRelation){
     if (nbRelations == maxRelations){
-        Couple** newRelations = new Couple[maxRelations+10];
-        for (int i=0; i < nbRelations; i++) newRelations[i] = relations[i];
-        Couple** oldRelations = relations;
+        Relation** newRelations = new Relation*[maxRelations+10];
+        for (unsigned int i=0; i < nbRelations; i++) newRelations[i] = relations[i];
+        Relation** oldRelations = relations;
         relations = newRelations;
         delete[] oldRelations;
         maxRelations += 10;
@@ -45,12 +45,12 @@ void RelationManager::ajouterRelation(Couple* newRelation){
 
 void RelationManager::supprimerRelation(Relation* supRelation){
     int pos = -1;
-    for (int i=1; i < nbRelations; i++){
+    for (unsigned int i=1; i < nbRelations; i++){
         if (relations[i] == supRelation) pos = i;
     }
 
     if (pos != -1){
-        for (int i=pos; i < nbRelations-1; i++){
+        for (unsigned int i=pos; i < nbRelations-1; i++){
             relations[i] = relations[i+1];
         }
         delete supRelation;
